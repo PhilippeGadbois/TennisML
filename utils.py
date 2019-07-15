f858d3bf3ef008919be5f5d4fb8c3b1a8022a3d0
PhilippeGadbois <52801766+PhilippeGadbois@users.noreply.github.com>
2019-07-15 17:32:06 -0400

    Set up SQL and Access connections

--- utils.py
+++ utils.py
@@ -1,5 +1,21 @@
 import pyodbc as pdb
 import pandas as pd
-import numpy as np
 
-query = "SELECT * FROM Stat.Players"
+
+def sql_connection(query):
+    conn = pdb.connect(driver='{SQL Server}', server='DESKTOP-E5TG60B\SQLEXPRESS', Trusted_Connection=True)
+    res = pd.read_sql(query, conn)
+    conn.close()
+    return res
+
+
+def access_connection(query):
+    conn_str = (
+        r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};'
+        r'DBQ=C:\Program Files (x86)\OnCourt\OnCourt.mdb;'
+        r'PWD=qKbE8lWacmYQsZ2'
+    )
+    conn = pdb.connect(conn_str)
+    res = pd.read_sql(query, conn)
+    conn.close()
+    return res
