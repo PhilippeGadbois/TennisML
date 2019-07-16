a47e6e28c2c8af1145ebb31d27d47c01377760e4
PhilippeGadbois <52801766+PhilippeGadbois@users.noreply.github.com>
2019-07-15 22:43:33 -0400

    Imported Players and Surface tables. Inserting dataframes now works.

--- database.py
+++ database.py
@@ -1,10 +1,15 @@
 import utils
 
-query = "SELECT * FROM Stat.Players"
-df = utils.sql_read(query)
+def update():
+    # Importing Players Table
+    query = "SELECT * FROM players_atp"
+    df = utils.access_read(query)
+    df = df[['ID_P', 'NAME_P', 'DATE_P', 'COUNTRY_P', 'PRIZE_P', 'POINT_P', 'RANK_P']]
+    df.columns = ['PlayerID', 'Name', 'Birthdate', 'Country', 'Prize', 'ATPPoints', 'ATPRank']
+    utils.sql_write(df, 'Players')
 
-query = "SELECT * FROM players_atp"
-df = utils.access_read(query)
-df = df[['ID_P', 'NAME_P', 'DATE_P', 'COUNTRY_P', 'PRIZE_P', ]]
-
-utils.sql_write(df, 'Stat.Players')
+    # Importing Surface Table
+    query = "SELECT * FROM courts"
+    df = utils.access_read(query)
+    df.columns = ['SurfaceID', 'Name']
+    utils.sql_write(df, 'Surface')
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import pyodbc as pdb
 import pandas as pd
+from sqlalchemy import create_engine
 
 
 def sql_read(query):
@@ -22,8 +23,9 @@ def access_read(query):
     conn.close()
     return res
 
-
+# You can create DSN in Control Panel > System and Security > Administrative Tools > ODBC Data Sources (64-bit)
+# User DSN > Add... > SQL Server > Input Name to SQLDSN > Server is your server name (DESKTOP-E5TG60B\SQLEXPRESS in my case)
+# Next > Next > Change the default database to: TennisML > Finish
 def sql_write(df, table):
-    conn = pdb.connect(driver='{SQL Server}', server='DESKTOP-E5TG60B\SQLEXPRESS', Trusted_Connection=True)
-    df.to_sql(table, conn)
-    conn.close()
+    engine = create_engine("mssql+pyodbc://SQLDSN")
+    df.to_sql(con=engine, name=table, schema='Stat', index=False, if_exists='replace')
